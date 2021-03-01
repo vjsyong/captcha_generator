d95d69b65d084e1947a7c4f005ac6951b255fb0b
Sean Yong <vjsyong@connect.ust.hk>
2021-03-01 13:58:13 +0800

    Fixed generation

--- captcha_generator.py
+++ captcha_generator.py
@@ -13,9 +13,10 @@ class Captcha:
         self.text = self.random_key(5)
         self.img = None
         self.font.append(ImageFont.truetype("PrimaSansMonoBT-Roman.otf", 45))
+        self.font.append(ImageFont.truetype("Prima_Sans_Mono_Bold_BT.ttf", 45))
         self.font.append(ImageFont.truetype("PrimaSansMonoBT-BoldOblique.otf", 45))
         self.font.append(ImageFont.truetype("PrimaSansBT-Oblique.otf", 45))
-        self.font.append(ImageFont.truetype("Prima_Sans_Mono_Bold_BT.ttf", 45))
+        
         
 
     def draw_frame(self):
@@ -36,35 +37,50 @@ class Captcha:
         (width, baseline), (offset_x, offset_y) = font.font.getsize(char)
         tailed_chars = "gjpqy"
         compact_chars = "aceimnorsuvwxz"
-        tall_chars = "bdfhklt0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        standard_chars = "bdfhklt0123456789ABCDEFGHIKLMNOPRSTUVWXYZ"
+        tall_chars = "JQ"
         if char in tailed_chars:
             return (w, ascent - offset_y + descent)
         elif char in compact_chars:
             return (w, ascent - offset_y)
+        elif char in standard_chars:
+            return (w, ascent - offset_y)
         else:
             return (w, ascent - offset_y)
-    
-    def draw_string(self, text):
+
+    def draw_string(self, text, text_file):
         lastwidth = 0 # Position to draw next char
+        char_list = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
         for i,char in enumerate(text):
-            random_font = random.choice(self.font)
+            rand = random.randint(0, 3)
+            random_font = self.font[rand]
             # w, h = random_font.getsize(char)
             w, h = self.get_char_size(random_font, char)
             offset = random_font.getoffset(char)
             tail_offset = 0
-            print(char, " offset", offset, "-- w, h =", w, h)
+            squeeze_offset = 0
+            # print(char, " offset", offset, "-- w, h =", w, h)
             tailed_chars = "gjpqy"
             if char in tailed_chars:
                 tail_offset = int(offset[1]/2)
-            lt = (lastwidth+10+offset[0] , self.height-h-13+tail_offset)
-            br = (lastwidth+ w+10+offset[0], self.height-11+tail_offset)
-            print(lt, br)
-            # cv2.rectangle(self.img, lt , br, (0,0,0), 1)
-        
+            if rand < 2: # squeeze boxes if not an oblique font
+                squeeze_offset = 2 
+            lt = (lastwidth+10+offset[0]+squeeze_offset, self.height-h-13+tail_offset) # lt - left top
+            br = (lastwidth+ w+10+offset[0]-squeeze_offset, self.height-11+tail_offset) # br - bottom right
+            x_center_scaled = (lt[0] + int(w/2)) / self.width
+            y_center_scaled = (lt[1] + int(h/2) + 1) / self.height
+            width_scaled = w / self.width
+            height_scaled = (h+2) / self.height
+            line = str(char_list.index(char)) + " " + str(x_center_scaled) + " " + str(y_center_scaled) + " " + str(width_scaled) + " " + str(height_scaled) + "\n"
+            text_file.write(line)
+
             # print(lastwidth)
             self.draw_char(self.img, (10 + lastwidth, -10), random_font, char)
+            # cv2.rectangle(self.img, lt , br, (0,0,0), 1)
+            # cv2.circle(self.img, (x_center, y_center), 2, (0, 0, 255), -1)
             # self.draw_char(self.img, (lastwidth + w, -10), random_font, char)
             lastwidth += (w-4)
+        text_file.close()
 
     def draw_line(self, img):
         # left_x = np.random.randint(0, self.width//4)
@@ -84,18 +100,20 @@ class Captcha:
         return key
     
     def generate(self):
+        filename = os.path.abspath(os.path.dirname(__file__)) + "/captchas" 
+        filename_jpg = filename + "/images/" + self.text + ".jpg"
+        filename_txt = filename + "/labels/" + self.text + ".txt"
+        text_file = open(filename_txt,"w+")
         self.draw_frame()
-        self.draw_string(self.text)
+        self.draw_string(self.text, text_file)
         self.draw_line(self.img)
         scaled = cv2.resize(self.img, (224, 64) , interpolation = cv2.INTER_AREA)
-        filename = os.path.abspath(os.path.dirname(__file__)) + "\\captchas\\" + self.text + ".jpg"
-        print(filename)
-        cv2.imshow("white", scaled)
-        cv2.imwrite(filename, scaled) 
-        cv2.waitKey()
+        # cv2.imshow("white", scaled)
+        cv2.imwrite(filename_jpg, scaled) 
+        # cv2.waitKey()
 
 if __name__ == '__main__':
     # text = "66nb2"  
-    while True:
-        c = Captcha(175, 50, "66nb2")
+    for i in range(2000):
+        c = Captcha(175, 50, "")
         c.generate()
